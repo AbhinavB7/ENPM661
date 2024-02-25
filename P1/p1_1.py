7e8c14eb00f39cdcc1e3f8ff8be2bf31d99cfe25
AbhinavB7 <babhinavmohan@gmail.com>
2024-02-25 06:25:20 -0400

    2

--- P1/p1_1.py
+++ P1/p1_1.py
@@ -1,27 +1,29 @@
 import numpy as np
 
+class Node:
+    def __init__(self, node_id, parent, state):
+        self.node_id = node_id
+        self.parent = parent
+        self.state = state
+        
 initial_state = np.array([[1, 4, 7], [2, 5, 8], [3, 6, 0]])       
 goal_state = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
 
-print('Initial state: \n', initial_state)
-print('Goal state: \n', goal_state)
+# print('Initial state: \n', initial_state)
+# print('Goal state: \n', goal_state)
+
 
 # /////////////////////////////////////////////////////////////////////////////
 
 # location of the blank tile
 
 def node_state(matrix):
-    # Using NumPy functions to find the indices where the value is 0
     indices = np.where(matrix == 0)
-
-    # The result is a tuple of arrays, so extracting the values
     i, j = indices[0][0], indices[1][0]
-
-    print (f'({i},  {j})')
     return i, j
 
-node_state(initial_state)
-node_state(goal_state)
+# node_state(initial_state)
+# node_state(goal_state)
 
 # //////////////////////////////////////////////////////////////////////////
 
@@ -77,38 +79,75 @@ not_visited = []
 # write a subfunction that uses backtracking to find the path from the initial state to the goal state
 
 def find_path(node_list, goal):
-    path = np.array([])
+    path = []
     current_node = node_list[-1]
     while not np.array_equal(current_node.state, initial_state):
-        path = np.append(path, current_node.state)
+        path.append(current_node.state.tolist())
         current_node = node_list[current_node.parent]
+    path.reverse()
     return path
 
 # //////////////////////////////////////////////////////////////////////////
 
+def bfs(initial_state, goal_state):
+    node_list = []
+    states_list = []
+    visited = []
+    not_visited = []
+    node_list.append(Node(0, -1, initial_state))
+    states_list.append(tuple(map(tuple, initial_state)))
+    not_visited.append(0)
+    node_id = 0
+    while not_visited:
+        current_node = not_visited.pop(0)
+        visited.append(current_node)
+        if np.array_equal(node_list[current_node].state, goal_state):
+            print('Goal state found')
+            store_nodes(node_list)
+            store_nodes_info(node_list)
+            store_path(node_list)
+            return node_list
+        else:
+            left, new_node = ActionMoveLeft(node_list[current_node].state)
+            if left:
+                new_node_tuple = tuple(map(tuple, new_node))
+                if new_node_tuple not in states_list:
+                    node_id += 1
+                    node_list.append(Node(node_id, current_node, new_node))
+                    states_list.append(new_node_tuple)
+                    not_visited.append(node_id)
+            right, new_node = ActionMoveRight(node_list[current_node].state)
+            if right:
+                new_node_tuple = tuple(map(tuple, new_node))
+                if new_node_tuple not in states_list:
+                    node_id += 1
+                    node_list.append(Node(node_id, current_node, new_node))
+                    states_list.append(new_node_tuple)
+                    not_visited.append(node_id)
+            up, new_node = ActionMoveUp(node_list[current_node].state)
+            if up:
+                new_node_tuple = tuple(map(tuple, new_node))
+                if new_node_tuple not in states_list:
+                    node_id += 1
+                    node_list.append(Node(node_id, current_node, new_node))
+                    states_list.append(new_node_tuple)
+                    not_visited.append(node_id)
+            down, new_node = ActionMoveDown(node_list[current_node].state)
+            if down:
+                new_node_tuple = tuple(map(tuple, new_node))
+                if new_node_tuple not in states_list:
+                    node_id += 1
+                    node_list.append(Node(node_id, current_node, new_node))
+                    states_list.append(new_node_tuple)
+                    not_visited.append(node_id)
 
+    return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# //////////////////////////////////////////////////////////////////////////
+  
 # store all the info about node index, parent, and state in the NodesInfo.txt file
 
-class Node:
-    def __init__(self, node_id, parent, state):
-        self.node_id = node_id
-        self.parent = parent
-        self.state = state
-        
+
 # store all the explored states in a list to Nodes.txt file
 
 def store_nodes(node_list):
@@ -131,14 +170,35 @@ def store_path(node_list):
         for node in path:
             f.write(f'{node}\n')
             
-
-
-
+            
 # node index is the index of the current node in the node_list
 # parent is the index of the parent node in the node_list
 # state is the current state of the node
 
 
+# //////////////////////////////////////////////////////////////////////////
 
+
+def main():
+    
+    # initial_state = np.array([[1, 4, 7], [2, 5, 8], [3, 6, 0]])
+    # init_mat = np.array([[4, 7, 0], [1, 2, 8], [3, 5, 6]])  # Test case 1
+    initial_state = np.array([[1, 4, 7], [2, 5, 8], [3, 0, 6]])  # Test case 2
+    # init_mat = np.array([[4, 5, 8], [2, 1, 7], [3, 6, 0]])  # Test case 3
+    goal_state = np.array([[1, 4, 7], [2, 5, 8], [3, 6, 0]])
+
+    print('Initial state: \n', initial_state)
+    
+    node_list = bfs(initial_state, goal_state)
+
+    if node_list:
+        # Print each step
+        for step, node in enumerate(node_list):
+            print(f'Step {step}:\n{np.array(node.state)}\n')
+    print('Goal state: \n', goal_state)
+    
+if __name__ == '__main__':
+    main()
+  
 
 
